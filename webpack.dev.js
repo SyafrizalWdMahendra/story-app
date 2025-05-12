edd1082560f7fe0621f2ea6862fa487bbae9e6ff
Mahen <mahennekkers27@gmail.com>
2025-05-12 07:32:06 +0700

    Fiveth deploy

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
@@ -14,7 +16,7 @@ module.exports = merge(common, {
     ],
   },
   devServer: {
-    static: path.resolve(__dirname, "dist"),
+    static: path.resolve(__dirname, "docs"),
     port: 9000,
     client: {
       overlay: {
@@ -25,6 +27,8 @@ module.exports = merge(common, {
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
     new GenerateSW({
       swDest: "sw.bundle.js",
     }),
